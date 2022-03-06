98e4fc8ebf07f216d4ff08538d2c6cad63c9fe84
1095813533 <1095813533@qq.com>
2022-03-06 23:16:30 +0800

    BLL添加中

--- BLL/检查单BLL.cs
+++ BLL/检查单BLL.cs
@@ -9,6 +9,21 @@ namespace BLL
 {
     public class 检查单BLL
     {
-        
+        public void 生成检查单(Model.检查单 jianchadan)  //生成检查单
+        {
+            new 检查单DAL().生成检查单(jianchadan);
+        }
+        public Model.检查单 检查单查询(Model.检查单 jianchadan) // 检查单查询
+        {
+            return new 检查单DAL().检查单查询(jianchadan);
+        }
+        public void 更新检查单(Model.检查单 jianchadan) // 检查单查询
+        {
+            new 检查单DAL().更新检查单(jianchadan);
+        }
+        public Model.检查单 检查单查询2(Model.检查单 jianchadan) // 检查单查询2，通过病案号查询
+        {
+            return new 检查单DAL().检查单查询2(jianchadan);
+        }
     }
 }
--- BLL/科室BLL.cs
+++ BLL/科室BLL.cs
@@ -10,6 +10,21 @@ namespace BLL
 {
     public class 科室BLL
     {
-       
+        public DataTable 科室刷新(Model.科室 keshi )
+        {
+            return new 科室DAL().科室刷新(keshi);
+        }
+        public DataTable 加载科室()
+        {
+            return new 科室DAL().加载科室();
+        }
+        public Model.科室 keshi_Query1(Model.科室 keshi) // 根据科室名获取科室信息
+        {
+            return new 科室DAL().keshi_Query1(keshi);
+        }
+        public Model.科室 keshi_Query2(Model.科室 keshi) // 根据科室编号获取科室信息
+        {
+            return new 科室DAL().keshi_Query2(keshi);
+        }
     }
 }
