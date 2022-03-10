acd9ce8a2ebfb2756ddfaf124f001c8524691f88
1095813533 <1095813533@qq.com>
2022-03-10 21:39:02 +0800

    BLL搞定

--- BLL/员工BLL.cs
+++ BLL/员工BLL.cs
@@ -11,6 +11,33 @@ namespace BLL
 {
     public class 员工BLL
     {
-        
+        public bool Login(Model.员工 yuangong)
+        {
+            return new 员工DAL().Login(yuangong);
+        }
+        public Model.员工 yuangong_Query1(Model.员工 yuangong) // 根据ID返回管理员信息
+        {
+            return new 员工DAL().yuangong_Query1(yuangong);
+        }
+        public Model.员工 yuangong_Query2(Model.员工 yuangong) // 根据科室和姓名获取员工编号
+        {
+            return new 员工DAL().yuangong_Query2(yuangong);
+        }
+        public bool 修改密码(Model.员工 yuangong)
+        {
+            return new 员工DAL().修改密码(yuangong);
+        }
+        public void yuangong_update(Model.员工 yuangong) // 
+        {
+            new 员工DAL().yuangong_update(yuangong);
+        }
+        public void yuangong_insert(Model.员工 yuangong)
+        {
+            new 员工DAL().yuangong_insert(yuangong);
+        }
+        public DataTable 科室员工(Model.员工 yuangong)
+        {
+            return new 员工DAL().科室员工(yuangong);
+        }
     }
 }
--- BLL/诊疗卡BLL.cs
+++ BLL/诊疗卡BLL.cs
@@ -10,6 +10,25 @@ namespace BLL
 {
     public class 诊疗卡BLL
     {
-        
+        public bool 诊疗卡查重(Model.诊疗卡 zhenliaoka)
+        {
+            return new 诊疗卡DAL().诊疗卡查重(zhenliaoka);
+        }
+        public bool 诊疗卡注册(Model.诊疗卡 zhenliaoka)
+        {
+            return new 诊疗卡DAL().诊疗卡注册(zhenliaoka);
+        }
+        public Model.诊疗卡 诊疗卡查询(Model.诊疗卡 zhenliaoka)
+        {
+            return new 诊疗卡DAL().诊疗卡查询(zhenliaoka);
+        }
+        public void 诊疗卡退卡(Model.诊疗卡 zhenliaoka)  // 诊疗卡退卡
+        {
+            new 诊疗卡DAL().诊疗卡退卡(zhenliaoka);
+        }
+        public void 诊疗卡换卡(Model.诊疗卡 zhenliaoka, string new_card)  // 诊疗卡换卡
+        {
+            new 诊疗卡DAL().诊疗卡换卡(zhenliaoka,new_card);
+        }
     }
 }
